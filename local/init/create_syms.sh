17b939467f793ba667a1cf232c230594f61148ea
aauren@shadowslab.com <shadowcat@malta.localdomain>
2015-01-02 16:38:12 -0800

    Created a script for initializing symlink setup

---
+++ local/init/create_syms.sh
@@ -0,0 +1,11 @@
+# This script creates the symlinks necessary to actually use the local files
+# contained in the parent directory.
+
+if [[ -d ${HOME}/dotfiles ]]; then
+  for DOT_FILE in $(ls -A "${HOME}"/dotfiles/local/ln); do
+    echo "linking ${DOT_FILE}"
+    ln -sf "${HOME}"/dotfiles/local/ln/"${DOT_FILE}" "${HOME}"/."${DOT_FILE}"
+  done
+fi
+
+ln -sf "${HOME}"/dotfiles/local/ssh_config "${HOME}"/.ssh/config
