eb6bd6d71ac04402264a668d451ff34d48fe23ca
Aaron U'Ren <aauren@users.noreply.github.com>
2015-02-04 17:12:43 -0800

    Made a few changes to allow for reuse

--- local/bin/window_activation_by_search.sh
+++ local/bin/window_activation_by_search.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-
-WINDOW_TITLES=$(wmctrl -l | grep -v "\-1" | sed -r 's/([^ ]+ +){3}(.*)/\2/g')
+ 
+WINDOW_LIST=$(wmctrl -l | grep -v "\-1")
+ 
+WINDOW_TITLES=$(echo "$WINDOW_LIST" | sed -r 's/([^ ]+ +){3}(.*)/\2/g')
 
 WINDOW_SELECTION=$(zenity --entry --title="Choose a Window" --text="${WINDOW_TITLES}")
-
-DESKTOP_NUMBER=$(wmctrl -l | grep -v "\-1" |grep -m 1 ${WINDOW_SELECTION} | sed -r 's/^[^ ]+ +([^ ]+) .*/\1/g')
-
+ 
+DESKTOP_NUMBER=$(echo "$WINDOW_LIST" | grep -m 1 ${WINDOW_SELECTION} | sed -r 's/^[^ ]+ +([^ ]+) .*/\1/g')
+ 
+echo "Selection: ${WINDOW_SELECTION} Desktop: ${DESKTOP_NUMBER}"
+ 
 xdotool search --desktop ${DESKTOP_NUMBER} --name ${WINDOW_SELECTION} windowactivate
