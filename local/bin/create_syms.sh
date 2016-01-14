b0f02841cf080f64ac78d836762b0ba568bbe42c
Aaron U'Ren <aauren@gmail.com>
2016-01-13 16:36:30 -0800

    create_syms.sh: Fixed and consolidated bash script

--- local/bin/create_syms.sh
+++ local/bin/create_syms.sh
@@ -1,20 +1,22 @@
+#!/bin/bash
 # This script creates the symlinks necessary to actually use the local files
 # contained in the parent directory.
 
-if [[ -d ${HOME}/dotfiles ]]; then
-  for DOT_FILE in $(ls -A "${HOME}"/dotfiles/local/ln); do
-    if [[ ! -d "${HOME}/.${DOT_FILE}" ]]; then
-      echo "linking ${DOT_FILE}"
-      ln -sf "${HOME}"/dotfiles/local/ln/"${DOT_FILE}" "${HOME}"/."${DOT_FILE}"
-    fi
-  done
-fi
+# if the dotfiles directory doesn't exist then there is really no reason to continue any of this
+[[ -d ${HOME}/dotfiles ]] || exit 1
+
+for DOT_FILE in "${HOME}/dotfiles/local/ln/"*; do
+	if [[ ! -d "${HOME}/.${DOT_FILE}" ]]; then
+		echo "linking ${DOT_FILE}"
+		ln -sf "${DOT_FILE}" "${HOME}/.$(basename DOT_FILE)"
+	fi
+done
 
 echo "linking ssh configurations"
-[[ -d ${HOME}/.ssh/config.d ]] || mkdir ${HOME}/.ssh/config.d
-ln -sf "${HOME}"/dotfiles/local/ssh_config "${HOME}"/.ssh/config.d/config.general
+[[ -d ${HOME}/.ssh/config.d ]] || mkdir "${HOME}/.ssh/config.d"
+ln -sf "${HOME}/dotfiles/local/ssh_config" "${HOME}/.ssh/config.d/config.general"
 
 echo "linking bin files"
 if [[ -d ${HOME}/bin ]]; then
-  ln -sf "${HOME}"/dotfiles/local/bin/* ${HOME}/bin
+  ln -sf "${HOME}"/dotfiles/local/bin/* "${HOME}"/bin
 fi
