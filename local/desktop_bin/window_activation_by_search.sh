bc8d39a4b412c9ff8c23e8b6f89b8684ca14d6bb
Aaron U'Ren <aauren@gmail.com>
2018-03-12 16:42:26 -0500

    window_activation_by_search.sh: rework to be more robust

--- local/desktop_bin/window_activation_by_search.sh
+++ local/desktop_bin/window_activation_by_search.sh
@@ -1,13 +1,20 @@
-#!/usr/bin/env bash
- 
-WINDOW_LIST=$(wmctrl -l | grep -v "\-1")
- 
-WINDOW_TITLES=$(echo "$WINDOW_LIST" | sed -r 's/([^ ]+ +){3}(.*)/\2/g')
+#!/bin/bash -x
+
+WINDOW_LIST="$(wmctrl -l | grep -vE "([^ ]+ +){3}(Desktop$|xfce4-panel|rainlendar)")"
+
+WINDOW_LIST_WITH_NUM="$(awk 'BEGIN{OFS=" "; col1=1}{print col1,$0; col1+=1}' <<<"${WINDOW_LIST}")"
+
+WINDOW_TITLES="$(sed -r 's/([0-9]+) ([^ ]+ +){3}(.*)/\1 \3/g' <<<"${WINDOW_LIST_WITH_NUM}")"
 
 WINDOW_SELECTION=$(zenity --entry --title="Choose a Window" --text="${WINDOW_TITLES}")
- 
-DESKTOP_NUMBER=$(echo "$WINDOW_LIST" | grep -m 1 ${WINDOW_SELECTION} | sed -r 's/^[^ ]+ +([^ ]+) .*/\1/g')
- 
-echo "Selection: ${WINDOW_SELECTION} Desktop: ${DESKTOP_NUMBER}"
- 
-xdotool search --desktop ${DESKTOP_NUMBER} --name ${WINDOW_SELECTION} windowactivate
+
+if [[ "${WINDOW_SELECTION}" =~ ^[0-9]+$ ]]; then
+	# If the user's input was a number we assume that they identified the window by ID
+	WIN_ID=$(grep -E "^${WINDOW_SELECTION} " <<<"${WINDOW_LIST_WITH_NUM}" | sed -r 's/^[^ ]+ +([^ ]+).*/\1/g')
+else
+	WIN_ID=$(grep -im 1 "${WINDOW_SELECTION}" <<<"${WINDOW_LIST}" | sed -r 's/^([^ ]+).*/\1/g')
+fi
+
+echo "Selection: ${WIN_ID}"
+
+xdotool windowactivate "$(printf "%d" "${WIN_ID}")"
