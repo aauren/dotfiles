c758fbb7b42467c13057d877ae33f02c0b94d4a3
Aaron U'Ren <aauren@gmail.com>
2025-10-18 14:16:41 -0500

    nvim.lua.plugins.treesitter.lua: remove TSUpdate to reduce treesitter spam on bootstrap

--- local/var_config/nvim.lua.plugins.treesitter.lua
+++ local/var_config/nvim.lua.plugins.treesitter.lua
@@ -3,7 +3,6 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		branch = 'master',
 		lazy = false,
-		build = ":TSUpdate",
 		config = function()
 			require'nvim-treesitter.configs'.setup {
 				-- A list of parser names, or "all" (the listed parsers MUST always be installed)
