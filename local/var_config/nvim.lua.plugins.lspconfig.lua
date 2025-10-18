9f7c2588d259921915b240ef05b9a85ea60f8784
Aaron U'Ren <aauren@gmail.com>
2025-10-18 12:03:53 -0500

    nvim.lua.plugins.lspconfig.lua: add Mason install logic for linting servers

--- local/var_config/nvim.lua.plugins.lspconfig.lua
+++ local/var_config/nvim.lua.plugins.lspconfig.lua
@@ -15,13 +15,33 @@ return {
 		},
 		config = function()
 			local mason_lspconfig = require("mason-lspconfig")
+			local mason_core = require("mason")
+			local mason_pkg = require("mason-core.package")
 			local mason_registry = require("mason-registry")
-
 			mason_registry.refresh()
+
+			-- Unfortunately Mason does come with an ensure_installed for packages similar to how mason-lspconfig does
+			-- for language servers. So we have to write this ourselves.
+			-- See: https://github.com/mason-org/mason.nvim/pull/1949#pullrequestreview-2853679405 for more details
+			---@param pkgs string[]
+			local function mason_install(pkgs)
+				local Command = require "mason.api.command"
+				local pkgs_to_install = {}
+				for _, pkg_identifier in ipairs(pkgs or {}) do
+					local pkg_name, _ = mason_pkg.Parse(pkg_identifier)
+					local ok, pkg = pcall(mason_registry.get_package, pkg_name)
+					if ok and not pkg:is_installed() and not pkg:is_installing() then
+						table.insert(pkgs_to_install, pkg_identifier)
+					end
+				end
+				if not vim.tbl_isempty(pkgs_to_install) then
+					Command.MasonInstall(pkgs_to_install)
+				end
+			end
 
 			-- Define the list of servers to be installed automatically by mason-lspconfig.
 			-- See `:help mason-lspconfig-ensure-installed` for more details.
-			local servers = {
+			local lsp_servers = {
 				"ansiblels",
 				"clangd",
 				"cmake",
@@ -39,13 +59,30 @@ return {
 				"yamlls",
 			}
 
+			local lint_servers = {
+				"ansible-lint",
+				"flake8",
+				"golangci-int",
+				"hadolint",
+				"luacheck",
+				"markdownlint",
+				"opa",
+				"shellcheck",
+				"tflint",
+				"tfsec",
+				"yamllint",
+			}
+
 			-- Setup mason so it can manage external tools.
-			require("mason").setup()
+			mason_core.setup()
+
+			-- Install non-lsp packages using Mason
+			mason_install(lint_servers)
 
 			-- Use mason-lspconfig to configure servers based on the `servers` list.
 			-- This will automatically pass the `on_attach` function and capabilities to each server.
 			mason_lspconfig.setup({
-				ensure_installed = servers,
+				ensure_installed = lsp_servers,
 			})
 
 			-- {{{ Configure golangci-lint
