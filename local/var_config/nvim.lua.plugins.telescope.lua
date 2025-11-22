a97092e5c24807d720f08b5dd65f1cc104257a8a
Aaron U'Ren <aauren@gmail.com>
2025-11-22 12:40:27 -0600

    nvim.lua.plugins.telescope.lua: add recent files picker and change ctrl-b to use that instead of buffers

--- local/var_config/nvim.lua.plugins.telescope.lua
+++ local/var_config/nvim.lua.plugins.telescope.lua
@@ -1,13 +1,18 @@
 return {
 	{
 		'nvim-telescope/telescope.nvim', branch = 'master',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'smartpde/telescope-recent-files',
+		},
 		config = function()
+			require('telescope').load_extension('recent_files')
 			local builtin = require('telescope.builtin')
+			local ts = require('telescope')
 			vim.keymap.set('n', '<c-p>', builtin.find_files, { desc = 'Telescope find files' })
 			vim.keymap.set('n', '<c-t>', builtin.lsp_dynamic_workspace_symbols, { desc = 'Telescope find workspace symbols' })
 			vim.keymap.set('n', '<c-q>', builtin.live_grep, { desc = 'Telescope live grep' })
-			vim.keymap.set('n', '<c-b>', builtin.buffers, { desc = 'Telescope buffers' })
+			vim.keymap.set('n', '<c-b>', ts.extensions.recent_files.pick, { desc = 'Telescope buffers' })
 			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 		end,
 	}
