61dda6eefc2b94d6aa4e7f266de91d20e3afe7b3
Aaron U'Ren <aauren@gmail.com>
2026-05-18 12:17:30 -0500

    nvim.lua.plugins.whichkey.lua: transition from classic to modern UI

--- local/var_config/nvim.lua.plugins.whichkey.lua
+++ local/var_config/nvim.lua.plugins.whichkey.lua
@@ -3,9 +3,8 @@ return {
 		"folke/which-key.nvim",
 		event = "VeryLazy",
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+			-- Use the modern preset; "classic" is also available if you prefer
+			preset = "modern",
 		},
 		keys = {
 			{
