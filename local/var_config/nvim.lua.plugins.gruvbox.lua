9aeef1a548d9e825fd941c572435a26916786d26
Aaron U'Ren <aauren@users.noreply.github.com>
2026-09-08 17:47:52 -0500

    colors(nvim): enable termguicolors to get more color depth

--- local/var_config/nvim.lua.plugins.gruvbox.lua
+++ local/var_config/nvim.lua.plugins.gruvbox.lua
@@ -6,6 +6,8 @@ return {
       config = function()
         -- Optionally configure and load the colorscheme
         -- directly inside the plugin declaration.
+        vim.opt.termguicolors = true
+        vim.opt.background = "dark"
         vim.g.gruvbox_material_enable_italic = true
         vim.g.gruvbox_material_background = "hard"
         vim.cmd.colorscheme('gruvbox-material')
